1943b03c24e856801806501bea2da3f9ec5365ec
gdolf <77839491+idolf@users.noreply.github.com>
2022-03-24 16:13:32 +0800

    Create InsertionSort.java

---
+++ sorts/InsertionSort.java
@@ -0,0 +1,8 @@
+package sorts;
+
+public class InsertionSort {
+  public static void Main(String args[]) {
+    System.out.println("反正时间复杂度还是像冒泡一样废，懒得写了");
+    System.out.println("The time complexity is the same as bubble sort, I'm lazy to write.");
+  }
+}
