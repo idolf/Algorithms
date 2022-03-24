0e871944d67cb9dd6b84c8090ddba0ded8175dc7
gdolf <77839491+idolf@users.noreply.github.com>
2022-03-24 15:27:05 +0800

    Add files via upload

---
+++ sorts/BubbleSort.java
@@ -0,0 +1,28 @@
+package sorts;
+
+// Time Complexity:
+// best: O(n) The array itself is ordered
+// worst: O(n^2)
+// average: O(n^2)
+
+// Chinese:
+// 时间复杂度
+// 最好: O(n) 数组本身有序时
+// 最差: O(n^2)
+// 平均: O(n^2)
+public class BubbleSort {
+    public static void bubbleSort(int []arr) {
+        int []result = new int[arr.length];
+        int t;
+        System.arraycopy(arr, 0, result, 0, arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    t = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = t;
+                }
+            }
+        }
+    }
+}
