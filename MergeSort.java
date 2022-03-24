4103eee5f570e16bcad5c42bd051120b09847c2b
gdolf <77839491+idolf@users.noreply.github.com>
2022-03-24 14:25:56 +0800

    Add files via upload

---
+++ MergeSort.java
@@ -0,0 +1,34 @@
+package sorts;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l >= r) return;
+        int mid = l + (r - l) / 2;
+
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid+1, r);
+        merge(arr, l, mid, r);
+    }
+
+    private static void merge(int []arr, int l, int m, int r) {
+        int []result = new int[r - l + 1];
+        int i = l, j = m + 1;
+        int idx = 0;
+        while (i <= m && j <= r) {
+            if (arr[i] < arr[j]) {
+                result[idx++] = arr[i++];
+            } else {
+                result[idx++] = arr[j++];
+            }
+        }
+        while (i <= m) {
+            result[idx++] = arr[i++];
+        }
+        while (j <= r) {
+            result[idx++] = arr[j++];
+        }
+        System.arraycopy(result, 0, arr, l, r - l + 1);
+    }
+}
